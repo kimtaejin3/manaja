ac5088dd690f8625bd79acfc4f4128b355c3e2cf
kimtaejin3 <rlaxowls1316@naver.com>
2024-10-12 21:46:29 +0900

    design: MainHeader 컴포넌트

--- src/components/headers/MainHeader.jsx
+++ src/components/headers/MainHeader.jsx
@@ -1,6 +1,8 @@
+import styles from "./MainHeader.module.scss";
+
 export default function MainHeader() {
   return (
-    <header>
+    <header className={styles.container}>
       <div>
         <h1>MANAJA</h1>
         <p>퇴근 시간 후딱 정하기</p>
