42c81e7b396593dc8e097e62422be130ab141958
kimtaejin3 <rlaxowls1316@naver.com>
2024-10-12 21:40:02 +0900

    markup: MainHeader 컴포넌트

---
+++ src/components/headers/MainHeader.jsx
@@ -0,0 +1,11 @@
+export default function MainHeader() {
+  return (
+    <header>
+      <div>
+        <h1>MANAJA</h1>
+        <p>퇴근 시간 후딱 정하기</p>
+      </div>
+      <div>...</div>
+    </header>
+  );
+}
