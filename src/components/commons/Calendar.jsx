0e46ec058c76e1ba18384e9d23de2774de8b4583
kimtaejin3 <rlaxowls1316@naver.com>
2024-10-14 10:43:46 +0900

    design: arrowIcon 캘린더에 추가

--- src/components/commons/Calendar.jsx
+++ src/components/commons/Calendar.jsx
@@ -1,12 +1,18 @@
 import styles from "./Calendar.module.scss";
+import arrowLeftIcon from "../../assets/arrow-left.svg";
+import arrowRightIcon from "../../assets/arrow-right.svg";
 
 export default function Calendar() {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <button>{"<"}</button>
+        <button>
+          <img src={arrowLeftIcon} alt="arrow_left_icon" />
+        </button>
         <span>10월</span>
-        <button>{">"}</button>
+        <button>
+          <img src={arrowRightIcon} alt="arrow_right_icon" />
+        </button>
       </div>
       <div></div>
     </div>
