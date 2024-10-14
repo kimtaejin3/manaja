6f1d447717bd45e1ff669795139c82e9c9f132d3
kimtaejin3 <rlaxowls1316@naver.com>
2024-10-14 15:21:06 +0900

    design: MeetingTimeField 컴포넌트 마크업 완료 및 일부 스타일링

--- src/components/fields/MeetingTimeField.jsx
+++ src/components/fields/MeetingTimeField.jsx
@@ -4,6 +4,23 @@ export default function MeetingTimeField() {
   return (
     <div className={styles.container}>
       <label>시간대 정하기</label>
+      <div className={styles.timeSelection}>
+        <div>
+          <select>
+            <option>오전 12시</option>
+            <option>오전 1시</option>
+          </select>
+          <span>아이콘</span>
+        </div>
+        <span>~</span>
+        <div>
+          <select>
+            <option>오전 12시</option>
+            <option>오전 1시</option>
+          </select>
+          <span>아이콘</span>
+        </div>
+      </div>
     </div>
   );
 }
