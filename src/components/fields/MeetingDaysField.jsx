e41a13bf014c0b7633247c13eceacd1d723a3f2a
kimtaejin3 <rlaxowls1316@naver.com>
2024-10-14 16:10:51 +0900

    update(MeetingDaysField.jsx): 불필요라 판단되는 문구 제거

--- src/components/fields/MeetingDaysField.jsx
+++ src/components/fields/MeetingDaysField.jsx
@@ -6,7 +6,6 @@ export default function MeetingDaysField({ sx }) {
     <div style={sx} className={styles.container}>
       <div className={styles.header}>
         <label>회의 후보 날짜 고르기</label>
-        <p>후보 날짜들을 모두 선택해주세요!</p>
       </div>
       <div className={styles.calendarField}>
         <Calendar />
