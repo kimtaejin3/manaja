b8821d9bcdf6ac45c156eadf9b1239d96979c6ea
kimtaejin3 <rlaxowls1316@naver.com>
2024-11-01 16:34:15 +0900

    markup: sidebar 컴포넌트 마크업

---
+++ src/components/sidebar/index.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import closeIcon from "../../assets/close.svg";
+import styles from "./Sidebar.module.scss";
+
+export default function Sidebar() {
+  return (
+    <div>
+      <CloseButton />
+      <ul>
+        <li>
+          <button>새로 만들기</button>
+        </li>
+        <li>
+          <Link to="/">도움말</Link>
+        </li>
+        <li>
+          <Link to="/">로그인</Link>
+          <p>
+            로그인 하시면 편리한 기능을 이용할 수 있어요! 도움말을 참고해주세요
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function CloseButton({ className }) {
+  return (
+    <button className={className}>
+      <img src={closeIcon} alt="사이드바 닫기 버튼" />
+    </button>
+  );
+}
