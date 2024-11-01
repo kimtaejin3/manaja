2338d1d8167285cefcb841359c1ee44b469fd8a4
kimtaejin3 <rlaxowls1316@naver.com>
2024-11-01 20:37:21 +0900

    feat: sidebarWrap 컴포넌트 구현

--- src/components/headers/MainHeader.jsx
+++ src/components/headers/MainHeader.jsx
@@ -1,31 +1,12 @@
 import styles from "./MainHeader.module.scss";
-import hamburgerIcon from "../../assets/hamburger.svg";
-import { useState } from "react";
-import Sidebar from "../sidebar";
 
 export default function MainHeader() {
-  const [isSidebarOpened, setIsSidebarOpened] = useState(false);
-
-  const openSidebar = () => {
-    setIsSidebarOpened(true);
-  };
-
-  const closeSidebar = () => {
-    setIsSidebarOpened(false);
-  };
-
   return (
     <header className={styles.container}>
       <div>
         <h1>MANAJA</h1>
         <p className={styles.subText}>회의 시간 후딱 정하기</p>
       </div>
-      <button className={styles.hamburgerButton} onClick={openSidebar}>
-        <img src={hamburgerIcon} alt="hamburger_icon" />
-      </button>
-      {isSidebarOpened && (
-        <Sidebar className={styles.sidebar} onClose={closeSidebar} />
-      )}
     </header>
   );
 }
