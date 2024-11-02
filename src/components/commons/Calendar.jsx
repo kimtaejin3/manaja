5d7239091ed38b771bd81120c266e6a06b347f33
kimtaejin3 <rlaxowls1316@naver.com>
2024-11-02 19:58:20 +0900

    update(Calendar.jsx): PrevButton, NextButton 생성

--- src/components/commons/Calendar.jsx
+++ src/components/commons/Calendar.jsx
@@ -1,28 +1,24 @@
 import styles from "./Calendar.module.scss";
 import arrowLeftIcon from "../../assets/arrow-left.svg";
 import arrowRightIcon from "../../assets/arrow-right.svg";
+import arrowLeftIcon_active from "../../assets/arrow-left_active.svg";
+import arrowRightIcon_active from "../../assets/arrow-right_active.svg";
+
+const dates = ["일", "월", "화", "수", "목", "금", "토"];
 
 export default function Calendar() {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <button>
-          <img src={arrowLeftIcon} alt="arrow_left_icon" />
-        </button>
-        <span>10월</span>
-        <button>
-          <img src={arrowRightIcon} alt="arrow_right_icon" />
-        </button>
+        <PrevButton />
+        <span>2024년 11월</span>
+        <NextButton />
       </div>
       <div className={styles.calendar}>
         <div className={styles.date}>
-          <span>일</span>
-          <span>월</span>
-          <span>화</span>
-          <span>수</span>
-          <span>목</span>
-          <span>금</span>
-          <span>토</span>
+          {dates.map((date) => (
+            <span key={date}>{date}</span>
+          ))}
         </div>
         <div className={styles.day}>
           {new Array(31).fill(0).map((val, index) => (
@@ -35,5 +31,27 @@ export default function Calendar() {
         </div>
       </div>
     </div>
+  );
+}
+
+function PrevButton({ isActive = true }) {
+  return (
+    <button>
+      <img
+        src={isActive ? arrowLeftIcon_active : arrowLeftIcon}
+        alt="arrow_left_icon"
+      />
+    </button>
+  );
+}
+
+function NextButton({ isActive = false }) {
+  return (
+    <button>
+      <img
+        src={isActive ? arrowRightIcon_active : arrowRightIcon}
+        alt="arrow_right_icon"
+      />
+    </button>
   );
 }
