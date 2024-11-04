a5a7a0062a9be4ddd1f79324a58f2bd339b02173
kimtaejin3 <rlaxowls1316@naver.com>
2024-11-04 22:24:54 +0900

    rename: detailPage에서 meetingPage로 이름 변경

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./components/layouts/MainLayout";
 import MainPage from "./pages/MainPage";
-import DetailPage from "./pages/DetailPage";
+import MeetingPage from "./pages/MeetingPage";
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/detail" element={<DetailPage />} />
+          <Route path="/meeting" element={<MeetingPage />} />
         </Routes>
       </BrowserRouter>
     </MainLayout>
