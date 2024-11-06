6826106cedfd45ed627ae9b666f5c1588c0189f8
kimtaejin3 <rlaxowls1316@naver.com>
2024-11-06 14:33:31 +0900

    fix: typo 수정

--- src/components/fields/MeetingDaysField.jsx
+++ src/components/fields/MeetingDaysField.jsx
@@ -2,9 +2,9 @@ import Calendar from "../shared/Calendar";
 import Field from "./Field";
 import styles from "./MeetingDaysField.module.scss";
 
-export default function MeetingDaysField({ sx }) {
+export default function MeetingDaysField() {
   return (
-    <Field className={styles.container} title="회의 후보날짜 선택">
+    <Field className={styles.container} title="회의 후보 날짜 선택">
       <div className={styles.calendarField}>
         <Calendar />
       </div>
